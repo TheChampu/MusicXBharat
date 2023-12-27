c6686f8ced63488f0f57aedcbbf204b0d83d8c1d
ᴄʜᴧᴍᴘᴜ <117635421+TheChampu@users.noreply.github.com>
2023-12-27 14:30:07 +0530

    Update __init__.py

--- TeamXBharat/__init__.py
+++ TeamXBharat/__init__.py
@@ -8,7 +8,7 @@ from SafoneAPI import SafoneAPI
 from .logging import LOGGER
 
 
-GB = ["https://telegra.ph/file/c06d6e3a3abacecf36427.mp4"]
+GB = ["https://telegra.ph/file/1885e904e8617e8a027c7.mp4"]
 
 OP = ["https://graph.org/file/ba8d4fb6d3410bbbe1dfc.jpg" , "https://graph.org/file/4c0be5dc3f741bbca04fa.jpg" , "https://graph.org/file/fc4ab06c07e1cc4ba96b0.jpg" , "https://graph.org/file/4466e0a16e28ac94212a2.jpg" , "https://graph.org/file/f2f1f01676b6a8ea5e8a2.jpg" , "https://graph.org/file/9a3e0e44a046af159dc6a.jpg" , "https://graph.org/file/6607c444b4aec00d6d591.jpg"]
 
